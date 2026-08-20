05dedd9145faca82da7dd659b474adfa541dcc26
chewst <234235836+chewst@users.noreply.github.com>
2026-08-20 14:26:36 +0800

    modify tiger to allow horizontal movement too

--- src/Tiger.cpp
+++ src/Tiger.cpp
@@ -1,26 +1,35 @@
 #include "Tiger.hpp"
 
-Tiger::Tiger(int gridX, int gridY, int patrolEndX, Color color, std::string name)
+Tiger::Tiger(int gridX, int gridY, int patrolEnd, Color color, std::string name, bool vertical)
     : Enemy(gridX, gridY, color, std::move(name), /* damage = */ 1),
-      m_patrolStartX(gridX),
-      m_patrolEndX(patrolEndX),
-      m_movingRight(patrolEndX > gridX) {}
+      m_patrolStart(vertical ? gridY : gridX),
+      m_patrolEnd(patrolEnd),
+      m_movingForward(patrolEnd > m_patrolStart),
+      m_vertical(vertical) {}
 
 void Tiger::takeTurn(const Grid& grid, int playerX, int playerY) {
     // unused for tiger
     (void)playerX;
     (void)playerY;
 
-    const int step = m_movingRight ? 1 : -1;
-    const int newX = getX() + step;
+    const int step = m_movingForward ? 1 : -1;
 
-    if (grid.isWalkable(newX, getY())) {
-        setPosition(newX, getY());
+    if (m_vertical) {
+        const int newY = getY() + step;
+        if (grid.isWalkable(getX(), newY)) {
+            setPosition(getX(), newY);
+        }
+    } else {
+        const int newX = getX() + step;
+        if (grid.isWalkable(newX, getY())) {
+            setPosition(newX, getY());
+        }
     }
 
-    if (getX() >= m_patrolEndX) {
-        m_movingRight = false;
-    } else if (getX() <= m_patrolStartX) {
-        m_movingRight = true;
+    const int current = m_vertical ? getY() : getX();
+    if (current >= m_patrolEnd) {
+        m_movingForward = false;
+    } else if (current <= m_patrolStart) {
+        m_movingForward = true;
     }
 }
--- src/Tiger.hpp
+++ src/Tiger.hpp
@@ -5,12 +5,13 @@
 // Patrols back and forth between its starting tile and a fixed point
 class Tiger : public Enemy {
 public:
-    Tiger(int gridX, int gridY, int patrolEndX, Color color, std::string name);
+    Tiger(int gridX, int gridY, int patrolEnd, Color color, std::string name, bool vertical = false);
 
-    void takeTurn(const Grid& grid, int, int) override;
+    void takeTurn(const Grid& grid, int playerX, int playerY) override;
 
 private:
-    int m_patrolStartX;
-    int m_patrolEndX;
-    bool m_movingRight;
+    int m_patrolStart;
+    int m_patrolEnd;
+    bool m_movingForward;
+    bool m_vertical;
 };
